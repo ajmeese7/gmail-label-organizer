d43666a7d57de3d8445f0a609fc8751d44b59eb5
Aaron Meese <ajmeese7@gmail.com>
2018-04-29 15:49:03 -0500

    Added files by upload

---
+++ organizer.js
@@ -0,0 +1,11 @@
+//document.body.style.border = "5px solid blue";
+// TODO: Make the labels draggable and the HTML changes reflect it
+//       Make the add-on remember user customization
+//       Remove a label's data when the label is no longer available
+
+var systemLabels = document.getElementsByClassName("TK")[0];
+console.log(systemLabels);
+
+
+// IDEA: Convert discovered labels into an array and switch the code? Complies with:
+//       https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/storage
