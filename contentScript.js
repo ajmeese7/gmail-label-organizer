7b1cd20d4d31fdba81d4fbec9a89d7e8fda59b99
Aaron Meese <ajmeese7@gmail.com>
2018-06-08 17:03:57 -0500

    Mostly fixed the problem
    Now it works when the number changes, but sometimes when you receive a new email everything gets screwed up. If anyone can figure this out, I would greatly appreciate it. Please report any unusual behavior on the issues page

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,7 @@
 (function() {
   // Running on page
   function modifyPage() {
-    // TODO: Make it refresh when innerHTML is modified (i.e. an email is deleted and the # is changed)
-    //       so the new order is restored (also somehow destroys IDs)
+    // IDEA: Make it refresh when innerHTML is modified?
 
     var storageItem = browser.storage.sync.get('order');
     storageItem.then((res) => {
@@ -15,8 +14,6 @@
       	elements.appendChild(document.getElementById(number + "").cloneNode(true));
       });
 
-      // IDEA: Run this portion of the code again when the innerHTML is modified by Gmail itself (^^top).
-      //       Also, re-implement the IDs by running organizer.js setIDs() again
       wrapper.innerHTML = null;
       wrapper.appendChild(elements);
     });
--- organizer.js
+++ organizer.js
@@ -2,10 +2,13 @@ labels();
 
 function labels() {
   var systemLabels = document.getElementsByClassName("TK")[0];
+  var children = systemLabels.children;
+  var childrenIDs = children[0].id;
   if (typeof systemLabels != 'undefined') {
-    var children = systemLabels.children;
-    for (var i = 0; i < children.length; i++) {
-      children[i].id = i + 1;
+    if (childrenIDs == "") {
+      for (var i = 0; i < children.length; i++) {
+        children[i].id = i + 1;
+      }
     }
 
     // This portion is a slightly modified version of modifyPage() in contentScript.js
@@ -22,6 +25,8 @@ function labels() {
       systemLabels.innerHTML = null;
       systemLabels.appendChild(elements);
     });
+
+    setTimeout(labels, 5000);
   } else {
     // Calls until the content is loaded
     setTimeout(labels, 100);
